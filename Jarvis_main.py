e7f3d84358dec4225a9f594a555b885dd1ec756f
ZERO HECKER <av9323915@gmail.com>
2024-11-30 09:25:50 +0530

    Update Jarvis_main.py

--- Jarvis_main.py
+++ Jarvis_main.py
@@ -1,1 +1,17 @@
-eating now I am edit code 
+import datetime
+import webbrowser
+import pyttsx3
+##from pywhatkit.sc import shutdown
+import speech_recognition 
+import requests 
+import bs4 import BeautifulSoup
+import os
+import pyautogui
+import random
+from plyer import notification
+from pygame import mixer
+###import speedtest-cli###
+import speedtest
+from playsound import playsound
+import time 
+
