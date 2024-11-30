4dc3ee41409fe8a48cbeb6aa0148d95878714347
ZERO HECKER <av9323915@gmail.com>
2024-11-30 09:35:05 +0530

    Update Jarvis_main.py

--- Jarvis_main.py
+++ Jarvis_main.py
@@ -220,3 +220,129 @@ def speak(audio):
 
 ##############################################
 
+
+
+                    elif "hello" in query:
+                    speak("Hello sir, how are you ?")
+                elif "i am fine" in query:
+                    speak("that's great, sir")
+                elif "how are you" in query:
+                    speak("Perfect, sir")
+                elif "thank you" in query:
+                    speak("you are welcome, sir")
+                    
+                ##elif "tired" in query:
+                    speak("Playing your favourite songs, sir")
+                    a = (1,2,3) # You can choose any number of songs (I have only choosen 3)
+                    b = random.choice(a)
+                    if b==1:
+                    webbrowser.open(https://youtube.com/playlist?list=RDZlFF3kJSoJs&playnext=1&si=T7elsIH7elwHYONV) 
+                    
+                    
+                  elif "open" in query:
+                      from Dictapp import openappweb
+                      openappweb(query)
+                  elif "close" in query:
+                      from Dictapp import closeappweb
+                      closeappweb(query)
+                      
+                    ##
+                    
+                elif "google" in query:
+                    from SearchNow import searchGoogle
+                    searchGoogle(query)
+                elif "youtube" in query:
+                     from SearchNow import searchYoutube
+                     searchYoutube(query)
+                elif "wikipedia" in query:
+                    from SearchNow import searchWikipedia
+                    searchWikipedia(query)
+ 
+                elif "news" in query:
+                   from NewsRead import latestnews
+                    latestnews()
+                    
+                    
+                  elif "calculate" in query:
+                     from Calculatenumbers  import WolfRamAlpha
+                     from      Calculatenumbers import Calc
+                     query = query.replace("calculate","")
+                     query = query.replace("jarvis","" )
+                      Calc(query)
+                      
+                      
+                  elif "whatsapp" in query:
+                      from Whatsapp import sendMessage
+                      sendMessage()
+                      
+ 
+                  ##elif "temperature" in query:
+                      search = "temperature in Gujarat"
+                      url = f"https://www.google.com/search?q={search}"
+                      r  = requests.get(url)
+                      data = BeautifulSoup(r.text,"html.parser")
+                      temp = data.find("div", class_ = "BNeawe").text
+                      speak(f"current{search} is {temp}")
+                  ##elif "weather" in query:
+                      search = "temperature in Gujarat"
+                      url = f"https://www.google.com/search?q={search}"
+                      r  = requests.get(url)
+                      data = BeautifulSoup(r.text,"html.parser")
+                      temp = data.find("div", class_ = "BNeaWE").text
+                      speak(f"current{search} is {temp}")
+                  
+                elif "set an alarm" in query:
+                    print("input time example:- 10 and 10 and 10")
+                    speak("Set the time")
+                    a = input("Please tell the time :- ")
+                    alarm(a)
+                    speak("Done,sir")
+                    
+                elif "pause" in query:
+                    pyautogui.press("k")
+                    speak("video paused")
+                elif "play" in query:
+                    pyautogui.press("k")
+                    speak("video played")
+                elif "mute" in query:
+                    pyautogui.press("m")
+                    speak("video muted")
+                
+                elif "volume up" in query:
+                    from keyboard import volumeup
+                    speak("Turning volume up,sir")
+                    volumeup()
+                elif "volume down" in query:
+                    from keyboard import volumedown
+                    speak("Turning volume down, sir")
+                    volumedown()
+                    
+                #####
+                  
+                  elif "the time" in query:
+                    strTime = datetime.datetime.now().strftime("%H:%M")    
+                    speak(f"Sir, the time is {strTime}")
+                  
+                  elif "finally sleep" in query:
+                      speak("Going to sleep,sir")
+                      exit()
+                      
+                  elif "remember that" in query:
+                      rememberMessage = query.replace("remember that","")
+                      rememberMessage = query.replace("jarvis","")
+                      speak("You told me to remember that"+rememberMessage)
+                      remember = open("Remember.txt","a")
+                      remember.write(rememberMessage)
+                      remember.close()
+                  elif "what do you remember" in query:
+                      remember = open("Remember.txt","r")
+                      speak("You told me to remember that" + remember.read())
+                      
+                  elif "shutdown the system" in query:
+                      speak("Are You sure you want to shutdown")
+                      shutdown = input("Do you wish to shutdown your computer? (yes/no)")
+                      if shutdown == "yes":
+                          os.system("shutdown /s /t 1")
+                  
+                      elif shutdown == "no":
+                          break
