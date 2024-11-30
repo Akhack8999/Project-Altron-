095203545608abee547e586174bc6887519d63d6
ZERO HECKER <av9323915@gmail.com>
2024-11-30 09:33:29 +0530

    Update Jarvis_main.py

--- Jarvis_main.py
+++ Jarvis_main.py
@@ -155,3 +155,68 @@ def speak(audio):
                          
   ##############################################
 
+
+                    
+                 elif "open" in query:   #EASY METHOD
+                      query = query.replace("open","")
+                      query = query.replace("jarvis","")
+                      pyautogui.press("super")
+                      pyautogui.typewrite(query)
+                      pyautogui.sleep(2)
+                      pyautogui.press("enter") 
+                    
+                    
+                  elif "internet speed" in query:
+                                      wifi  = speedtest.Speedtest()
+                                      upload_net = wifi.upload()/1048576         #Megabyte = 1024*1024 Bytes
+                                      download_net = wifi.download()/1048576
+                                      print("Wifi Upload Speed is", upload_net)
+                                      print("Wifi download speed is ",download_net)
+                                      speak(f"Wifi download speed is {download_net}")
+                                      speak(f"Wifi Upload speed is {upload_net}")
+                                      
+                                      
+                  elif "ipl score" in query:
+                                      from plyer import notification  #pip install plyer
+                                      import requests #pip install requests
+                                      from bs4 import BeautifulSoup #pip install bs4
+                                      url = "https://www.cricbuzz.com/"
+                                      page = requests.get(url)
+                                      soup = BeautifulSoup(page.text,"html.parser")
+                                      team1 = soup.find_all(class_ = "cb-ovr-flo cb-hmscg-tm-nm")[0].get_text()
+                                      team2 = soup.find_all(class_ = "cb-ovr-flo cb-hmscg-tm-nm")[1].get_text()
+                                      team1_score = soup.find_all(class_ = "cb-ovr-flo")[8].get_text()
+                                      team2_score = soup.find_all(class_ = "cb-ovr-flo")[10].get_text()
+                  
+                                      a = print(f"{team1} : {team1_score}")
+                                      b = print(f"{team2} : {team2_score}")
+                  
+                                      notification.notify(
+                                          title = "IPL SCORE :- ",
+                                          message = f"{team1} : {team1_score}\n {team2} : {team2_score}",
+                                          timeout = 15
+                                      )            
+                                      
+                  elif "play a game" in query:
+                    from game import game_play
+                    game_play()
+                                      
+                  elif "screenshot" in query:
+                                       import pyautogui #pip install pyautogui
+                                       im = pyautogui.screenshot()
+                                       im.save("ss.jpg")
+                                       
+                                    
+                  elif "click my photo" in query:
+                                      pyautogui.press("super")
+                                      pyautogui.typewrite("camera")
+                                      pyautogui.press("enter")
+                                      pyautogui.sleep(2)
+                                      speak("SMILE")
+                                      pyautogui.press("enter")
+                  
+                  
+                  #HOPE YOU LIKED IT 
+
+##############################################
+
