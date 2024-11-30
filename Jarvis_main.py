254bc44734dd2c6465034003935511a100c84744
ZERO HECKER <av9323915@gmail.com>
2024-11-30 09:26:46 +0530

    Update Jarvis_main.py

--- Jarvis_main.py
+++ Jarvis_main.py
@@ -15,3 +15,28 @@ import speedtest
 from playsound import playsound
 import time 
 
+
+#Paste this just below your import files
+for i in range(3):
+    a = input("Enter Password to open Jarvis :- ")
+    pw_file = open("password.txt","r")
+    pw = pw_file.read()
+    pw_file.close()
+    if (a==pw):
+        print("WELCOME SIR ! PLZ SPEAK [WAKE UP] TO LOAD ME UP")
+        break
+    elif (i==2 and a!=pw):
+        exit()
+
+    elif (a!=pw):
+        print("Try Again")
+        
+    from INTRO import play_gif
+                          play_gif
+                         #paste this just below the password
+
+engine = pyttsx3.init("sapi5")
+voices = engine.getProperty("voices")
+engine.setProperty("voice", voices[0].id)
+engine.setProperty("rate",170)
+
